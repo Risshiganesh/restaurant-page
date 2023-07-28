08956e811ea24c34c31aca639c3ac59889e2e5d6
Risshiganesh Villalan <risshiganesh1997@gmail.com>
2023-07-29 05:27:38 +0800

    Import CSS to index.js

--- src/index.js
+++ src/index.js
@@ -1,3 +1,5 @@
+import './style.css';
+
 import createMainBody from "./main-load";
 
 import createHomeElements from "./home";
--- webpack.config.js
+++ webpack.config.js
@@ -15,4 +15,12 @@ module.exports = {
       },
     ],
   },
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ['style-loader', 'css-loader'],
+      },
+    ],
+  },
 };
